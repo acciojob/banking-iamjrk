5703835a0bb30c88a0f1d098b91848d5faaf1706
Jyoti Ranjan Kumar <kumar.jyotiranjan575@gmail.com>
2023-06-12 17:07:54 +0530

    Attempt 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,17 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception
+    {
+        try {
+            CurrentAccount ca1 = new CurrentAccount("Rocky", 5500, "AAA");
+            SavingsAccount sa1 = new SavingsAccount("Rockysa1",60000,100000,2);
+            System.out.println(sa1.getBalance());
+            System.out.println(sa1.getSimpleInterest(3));
+            System.out.println(sa1.getCompoundInterest(2,3));
+            System.out.println(sa1.generateAccountNumber(5,1));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
